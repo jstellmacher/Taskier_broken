522bed0464a4f29a66ba76ae72c4cb35a3a674ac
Jai <jaijai.stell@gmail.com>
2023-06-12 05:03:26 -0700

    uhhhh

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 import { FiInfo, FiHome, FiCalendar, FiSmile } from 'react-icons/fi';
+import { GiExitDoor } from 'react-icons/gi';
 
 const containerStyles = 'flex items-center justify-between p-4';
 const itemStyles = 'flex items-center';
-const logoStyles = 'h-8 w-8 mr-2'; // Adjusted size for larger icons
+const logoStyles = 'h-8 w-8 mr-2 hover:animate-pulse'; // Adjusted size for larger icons and added hover:pulse class
 const textStyles = 'font-bold';
+const logoutStyles = 'hover:animate-pulse';
 
 function NavBar() {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    if (window.confirm('Are you sure you want to logout?')) {
+      // Perform logout action here
+      // For example, you can clear the session, remove tokens, or reset user data
+
+      // After logout, redirect the user to the login page or home page
+      history.push('/login');
+    }
+  };
+
   return (
     <nav>
       <ul className={containerStyles}>
@@ -27,6 +41,9 @@ function NavBar() {
           <NavLink to="/about" activeClassName="text-blue-500">
             <FiSmile className={logoStyles} />
           </NavLink>
+          <div className={logoutStyles} onClick={handleLogout}>
+            <GiExitDoor className={logoStyles} />
+          </div>
         </li>
       </ul>
     </nav>
